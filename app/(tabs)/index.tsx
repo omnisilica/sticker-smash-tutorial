d8446ec14c437aef4d37d4718670c641bcce05b2
omnisilica <oxmaverix@gmail.com>
2026-01-17 16:59:15 -0500

    Added Image Picker

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,54 @@
+import Button from "@/app/components/Button";
+import ImageViewer from "@/app/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
+import { StyleSheet, View } from "react-native";
+
+const PlaceHolderImage = require("@/assets/images/background-image.png");
+
+export default function Index() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceHolderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+});
